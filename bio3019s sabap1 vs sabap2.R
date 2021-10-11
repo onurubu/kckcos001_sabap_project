b84165ae4c43f1563918b321e6cbecf56844f421
onurubu <onurubu@gmail.com>
2021-10-11 11:29:32 +0200

    Initial commit of all pre existing files I had for the SABAP project with Arjun Amar

---
+++ bio3019s sabap1 vs sabap2.R
@@ -0,0 +1,42 @@
+
+
+setwd("C:/Users/01429358/Documents/R")
+
+ME <- read.csv("~/Teaching/BIO2019S Quantitative Biology/BIO3019S projects/BIO3019S project Amar/142ME.csv")
+
+
+sabap12all <- read.csv("~/Teaching/BIO2019S Quantitative Biology/BIO3019S projects/BIO3019S project Amar/merged_SABAP_onlySA_WORKS.csv", header=TRUE, sep=",", fill=TRUE, row.names=NULL, strip.white=TRUE)
+str(sabap12all)
+sabap12all$periodcat <- as.factor(sabap12all$period)
+
+
+#speciesnames=unique(sabap12all$common.name)   #make a list of all the species name you need to loop through.
+#modelresults=NULL  #this is an empty object where your results will be saved
+
+
+
+#here you subset the different species
+  VEdata=subset(sabap12all, common.name == "Verreaux'sEagle") #subset species data
+  MEdata=subset(sabap12all, common.name == "MartialEagle")
+  MartialEagle
+  
+  
+  
+  M1=glm(cbind(sightings, (cards-sightings))~periodcat, family=quasibinomial, data=VEdata) 
+  summary(M1)
+  plot(allEffects(M1)) # this plots out the effects
+  
+  # the emmeans gives you the back transformed reporting rates for each period category
+  emm <- emmeans(M1, "periodcat", type="response")
+  emm
+  eff_size(emm, sigma = sigma(M1), edf = df.residual(M1))#
+  
+  M2=glm(cbind(sightings, (cards-sightings))~periodcat, family=quasibinomial, data=MEdata) 
+  summary(M2)
+  plot(allEffects(M2))
+  
+  
+  emm <- emmeans(M2, "periodcat", type="response")
+  emm
+  eff_size(emm, sigma = sigma(M2), edf = df.residual(M2))#
+  
